63eeefaa54484dfa10a75b8c14081981ee3da0dc
ChiKien_Luu <luukien2910@gmail.com>
2025-04-04 21:59:49 +0700

    CSharpOOP

---
+++ CSharpClassed/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+        // Constructor
+        public Car(string model, string color, int year)
+        {
+            this.model = model;
+            this.color = color;
+            this.year = year;
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car("Mustang", "red", 2025);
+            Console.WriteLine("My car:" + myObj.model + "\nThe color of my car:" + myObj.color + "\nYear:" + myObj.year);
+        }
+
+    }
+
+
+
+}
